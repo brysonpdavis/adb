d5f1513f3ff3da6d826cdf8a84fb28566a53d147
brysonpdavis <bryson.davis@discordapp.com>
2025-04-30 10:59:24 -0400

    fix footer width

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,7 +2,7 @@
 	import { BRYSON_LINKEDIN } from '$lib/constants';
 </script>
 
-<footer class="bg-base-300 relative mt-4 flex h-fit w-screen flex-row items-center p-4">
+<footer class="bg-base-300 relative mt-4 flex h-fit flex-row items-center p-4">
 	<div class="text-neutral m-auto text-center text-sm leading-loose text-balance">
 		Website design by me :) * Coded by my husband (take a look at his
 		<a href={BRYSON_LINKEDIN} target="_blank" class="hover:text-primary underline">LinkedIn</a>!)
